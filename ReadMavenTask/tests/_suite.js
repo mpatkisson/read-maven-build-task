4a204d754e1f2a1bf9f1493cd7005ea5b682c10f
Michael Atkisson <mpatkisson@gmail.com>
2019-10-07 06:51:32 -0700

    Add test scaffolding

---
+++ ReadMavenTask/tests/_suite.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+describe('Sample task tests', function () {
+    before(function () {
+    });
+    after(() => {
+    });
+    it('should succeed with simple inputs', function (done) {
+        // Add success test here
+        done();
+    });
+});
